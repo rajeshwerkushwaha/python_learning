e56a1abd41522f041c4beb3d5c8ca36396e9c977
Rajesh Kumar <rajeshwerkushwaha@gmail.com>
2019-09-24 17:32:00 +0100

    new files updated

---
+++ detect_face/detect_face.py
@@ -0,0 +1,18 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('/anaconda3/pkgs/libopencv-3.4.2-h7c891bd_1/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+
+img = cv2.imread('groupphoto.jpg')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=5)
+
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+#resized = cv2.resize(img, (int(img.shape[1]/7), int(img.shape[0]/7)))
+cv2.imshow("FacesImg", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
