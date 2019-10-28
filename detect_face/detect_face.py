24fc92df0ab7e58739e995d91e73be72b81e41cc
Rajesh Kumar <rajeshwerkushwaha@gmail.com>
2019-10-28 17:32:00 +0000

    new files updated

--- detect_face/detect_face.py
+++ detect_face/detect_face.py
@@ -1,19 +1,25 @@
-import cv2
+import cv2 as cv
 
-face_cascade = cv2.CascadeClassifier('/anaconda3/pkgs/libopencv-3.4.2-h7c891bd_1/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier('/anaconda3/pkgs/libopencv-3.4.2-h7c891bd_1/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
 #face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 #eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
 
-img = cv2.imread('groupphoto.jpg')
+img = cv.imread('groupphoto.jpg')
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(img)
 
-faces = face_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=5)
+# cv.imshow("FacesImg", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#resized = cv2.resize(img, (int(img.shape[1]/7), int(img.shape[0]/7)))
-cv2.imshow("FacesImg", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# faces = face_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=5)
+
+# for (x,y,w,h) in faces:
+#     img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+# #resized = cv2.resize(img, (int(img.shape[1]/7), int(img.shape[0]/7)))
+# cv2.imshow("FacesImg", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
