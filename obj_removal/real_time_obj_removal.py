a5207ca5aee09500ef2dda7fe3a3a36c01613afa
Rajesh Kumar <rkumar@groupon.com>
2020-04-27 17:32:00 +0100

    new files updated

--- obj_removal/real_time_obj_removal.py
+++ obj_removal/real_time_obj_removal.py
@@ -1,95 +1,17 @@
+import cv2 as cv
 import numpy as np
-import cv2
 
-cap = cv2.VideoCapture(0)
+img = np.zeros((400, 600, 1), dtype=np.uint8)
+cv.imshow('image', img)
+sub_img = np.zeros([100,150,1],dtype=np.uint8)
+sub_img.fill(255)
+cv.imshow('sub image', sub_img)
 
-# Define the codec and create VideoWriter object
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-size = (width, height)
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, size)
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret==True:
-        frame = cv2.flip(frame,0)
-
-        # write the flipped frame
-        out.write(frame)
-
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
-
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+copy_img = img.copy()
+copy_img[100:100+sub_img.shape[0], 100:100+sub_img.shape[1]] = sub_img
+cv.imshow('merge image', copy_img)
 
 
-
-# import cv2 as cv
-# import numpy as np
-
-# algo = 'MOG2' # you can chose any algo between MOG2 or KNN
-# if algo == 'MOG2':
-#     backSub = cv.createBackgroundSubtractorMOG2(history = 50,
-#                                                 varThreshold = 16,
-#                                                 detectShadows = True)
-# elif algo == 'KNN':
-#     backSub = cv.createBackgroundSubtractorKNN()
-
-# cap = cv.VideoCapture(0)  # capture webcam object
-# print(dir(cap.get.__getattribute__))
-
-# if not cap.isOpened:
-#     print('Unable to open: ' + args.input)
-#     exit(0)
-# while True:
-#     ret, frame = cap.read()       # start getting feed from webcam
-
-
-#     if frame is None:
-#         break
-    
-#     fgMask = backSub.apply(frame) # apply above provided Background Subtraction algo to each frame
-    
-#     fgMask_new = fgMask[:,:,np.newaxis]
-#     fgMask_new = np.where(fgMask_new==1, [1,1,1], [0,0,0])
-
-# #     print(frame.shape, fgMask_new.shape)
-# #     sub = cv.subtract(frame, fgMask_new)
-
-#     cv.imshow('Frame', frame)
-#     cv.imshow('FG Mask', fgMask)
-# #     cv.imshow('Subtract', sub)
-    
-    
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
-        
-# cv.destroyAllWindows()
-
-
-
-# # a = np.array([[1, 2, 3],
-# # ...           [2, 2, 4],
-# # ...           [3, 1, 5]])
-
-# # b = np.array([[0, 0, 0],
-# # ...           [0, 1, 1],
-# # ...           [0, 0, 0]])
-
-# # c = np.array([[1, 1, 1],
-# # ...           [0, 1, 1],
-# # ...           [0, 0, 0]])
-
-
-
-
-
+k = cv.waitKey(0) & 0xFF
+if k == ord('q'):
+    cv.destroyAllWindows()
