4e5c98f20233dec6ce3a6ff8c6568bdc532ff2ef
Rajesh Kumar <rkumar@groupon.com>
2020-04-30 17:32:01 +0100

    new files updated

--- opencv/face_detection.py
+++ opencv/face_detection.py
@@ -33,19 +33,3 @@ while True:
 
 cap.release()
 cv.destroyAllWindows()
-
-
-
-# ❯ ls
-# __init__.py                                haarcascade_fullbody.xml
-# __pycache__                                haarcascade_lefteye_2splits.xml
-# haarcascade_eye.xml                        haarcascade_licence_plate_rus_16stages.xml
-# haarcascade_eye_tree_eyeglasses.xml        haarcascade_lowerbody.xml
-# haarcascade_frontalcatface.xml             haarcascade_profileface.xml
-# haarcascade_frontalcatface_extended.xml    haarcascade_righteye_2splits.xml
-# haarcascade_frontalface_alt.xml            haarcascade_russian_plate_number.xml
-# haarcascade_frontalface_alt2.xml           haarcascade_smile.xml
-# haarcascade_frontalface_alt_tree.xml       haarcascade_upperbody.xml
-# haarcascade_frontalface_default.xml
-# ❯ pwd
-# /Users/rkumar/.virtualenvs/cv/lib/python3.7/site-packages/cv2/data
