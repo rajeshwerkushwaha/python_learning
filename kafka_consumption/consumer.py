387bf083793151eb5aacf2dfac467d06d44f9c1c
Rajesh Kumar <rajeshwerkushwaha@gmail.com>
2020-06-10 17:32:00 +0100

    new files updated

--- kafka_consumption/consumer.py
+++ kafka_consumption/consumer.py
@@ -2,14 +2,17 @@ from kafka import KafkaConsumer
 from json import loads
 
 consumer = KafkaConsumer(
-    'numtest',
-     bootstrap_servers=['localhost:9092'],
+    'janus-tier1_snc1',
+     # bootstrap_servers=['localhost:9092'],
+     bootstrap_servers=['kafka.dub1:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
-     group_id='my-group',
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
+     # group_id='janus-tier1_snc1',
+     # topic='janus-tier1_snc1',
+     value_deserializer=lambda x: loads(x.decode('utf-8'))
 
 
 for message in consumer:
     message = message.value
-    print('read {} from numtest kafka topic'.format(message))
+    # print('read {} from numtest kafka topic'.format(message))
+    print(message)
